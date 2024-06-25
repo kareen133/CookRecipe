68cf9e2407adccc2cf906a8b78084df019831671
kareen133 <kareen3alkhateeb3@gmail.com>
2024-06-25 23:33:02 +0300

    this is cookrecipe

---
+++ main.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    const toggleMenu =document.querySelector(".toggleMenu");
+    const Navigation =document.querySelector(".Navigation");
+    toggleMenu.classList.toggle("active");
+    Navigation.classList.toggle("active");
+}
+
